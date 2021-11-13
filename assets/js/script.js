05e8a71aad411d5c6807e3f4cb0648d5f5373365
Christopher Speltz <chrisspeltz@msn.com>
2021-11-13 12:30:50 -0600

    created functiont to audit time and determine color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // declaration for current time
-let currentTime = moment().format("dddd, MMMM Do");
-// console.log(currentTime);
+let currentDate = moment().format("dddd, MMMM Do");
+// console.log(currentDate);
 
 // declaration for current hour
 let currentHour = moment().format("H");
@@ -17,25 +17,39 @@ let plannerList = [
   { hour: "4PM", plannedEvent: "" },
   { hour: "5PM", plannedEvent: "" },
 ];
-// add current date to p tag header
-$("#currentDay").append(currentTime);
+// add current date  header
+$("#currentDay").append(currentDate);
 
-// create planner 
+// function to audit time and determine color
+function auditHour(hour) {
+    let presentTime = moment(currentHour, "H A");
+    let hourSlot = moment(hour, "H A");
+  
+    if (presentTime.isBefore(hourSlot) === true) {
+      return "future";
+    } else if (presentTime.isAfter(hourSlot) === true) {
+      return "past";
+    } else {
+      return "present";
+    }
+  }
+
+//function to create planner
 plannerList.forEach(function (plannerListHour) {
-
+  
   let label = plannerListHour.hour;
+
+  let auditedEvent = auditHour(label);
 
   // declaration for planner list, including the hour, entered text and save button
   let eventList =
-    '<div class="time-block" id="'+'"><div class="row no-gutters input-group"><div class="col-sm-2 col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
-    label +
-    // '</div><textarea class="form-control ' +
-    // ' description">' +
-    // '</textarea><div class="col-sm-2 col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="far fa-save"></i></button></div></div></div>';
+    '<div class="time-block"><div class="row no-gutters input-group"><div class="col-sm-2 col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' + label +
+    '</div><textarea class="form-control ' + auditedEvent + ' description">' + plannerListHour.plannedEvent + '</textarea><div class="col-sm-2 col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="far fa-save"></i></button></div></div></div>';
 
   // add rows to page
   $(".container").append(eventList);
 });
+
 
 // day planner save button clicked
 $(".saveBtn").on("click", function () {
